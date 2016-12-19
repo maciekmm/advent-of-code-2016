ce55765d48a157b25dd5bf91457dacba0e94bef1
Maciej Mionskowski <business@maciekmm.net>
2016-12-19 16:07:48 +0100

    Added Day 19 part 2

--- 19-an_elephant_named_joseph/19.go
+++ 19-an_elephant_named_joseph/19.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -9,5 +10,7 @@ func main() {
 	input := int64(3005290)
 	//https://www.youtube.com/watch?v=uCsD3ZGzMgE
 	//Josephus problem
-	fmt.Println(input<<1 + 1 - 1<<uint(len(strconv.FormatInt(input, 2))))
+	fmt.Println("Part 1:", input<<1+1-1<<uint(len(strconv.FormatInt(input, 2))))
+	z := int64(math.Pow(float64(3), float64(len(strconv.FormatInt(input, 3))-1))) //we all love logarithms ;D
+	fmt.Println("Part 2:", int64(math.Max(float64(input-z), float64(2*input-3*z))))
 }
